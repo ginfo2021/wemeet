5a565eff69a10abeced1e0caa25e393ed38ddac9
accelerex-developer <51917896+accelerex-developer@users.noreply.github.com>
2020-08-19 16:19:16 +0100

    initial

---
+++ src/test/java/com/wemeet/dating/DatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemeet.dating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
