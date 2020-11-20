cc63c3bc5ad6d89e462fdae34b765070beea5246
udoriekpin <udorifriday@gmail.com>
2020-11-20 20:45:55 +0100

    fixes

--- src/main/java/com/wemeet/dating/config/AWSConfig.java
+++ src/main/java/com/wemeet/dating/config/AWSConfig.java
@@ -1,14 +1,13 @@
 package com.wemeet.dating.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.core.io.s3.SimpleStorageResourceLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 
 @Configuration
@@ -18,22 +17,23 @@ public class AWSConfig {
     private WemeetConfig wemeetConfig;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
-        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
-        builder.withCredentials(DefaultAWSCredentialsProviderChain.getInstance());
-        builder.setRegion(wemeetConfig.getRegion());
-        AmazonS3 amazonS3 = builder.build();
-        return amazonS3;
+    public AmazonS3 amazonS3() {
+        InstanceProfileCredentialsProvider provider
+                = new InstanceProfileCredentialsProvider(true);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(provider)
+                .withRegion(Regions.EU_WEST_1)
+                .build();
     }
 
     @Bean
-    public SimpleStorageResourceLoader simpleStorageResourceLoader(AmazonS3 amazonS3Client) {
-        return new SimpleStorageResourceLoader(amazonS3Client);
+    public SimpleStorageResourceLoader simpleStorageResourceLoader(AmazonS3 amazonS3) {
+        return new SimpleStorageResourceLoader(amazonS3);
     }
 
-    @Bean
-    @Primary
-    public AWSCredentialsProvider awsCredentialsProvider() {
-        return new DefaultAWSCredentialsProviderChain();
-    }
+//    @Bean
+//    @Primary
+//    public AWSCredentialsProvider awsCredentialsProvider() {
+//        return new DefaultAWSCredentialsProviderChain();
+//    }
 }
