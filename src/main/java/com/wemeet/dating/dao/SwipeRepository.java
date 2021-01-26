625c6bb37ea269678b586b159721f0797306595f
olamide <laleyeolamide@yahoo.com>
2021-01-26 16:55:07 +0100

    refactor swipe suggestion

--- src/main/java/com/wemeet/dating/dao/SwipeRepository.java
+++ src/main/java/com/wemeet/dating/dao/SwipeRepository.java
@@ -26,14 +26,14 @@ public interface SwipeRepository extends BaseRepository<Swipe, Long> {
                     " ON A.swiper_id = B.swipee_id " +
                     " WHERE A.swiper_id = B.swipee_id " +
                     " AND B.swiper_id = A.swipee_id " +
-                    " AND  A.swiper_id = :userId AND A.TYPE = :swipeType",
+                    " AND  A.swiper_id = :userId AND A.TYPE = :swipeType AND B.TYPE = :swipeType",
             countQuery
                     = "SELECT COUNT(*) FROM( SELECT A.swipee_id AS id FROM swipe A " +
                     " INNER JOIN swipe B " +
                     " ON A.swiper_id = B.swipee_id " +
                     " WHERE A.swiper_id = B.swipee_id " +
                     " AND B.swiper_id = A.swipee_id " +
-                    " AND  A.swiper_id = :userId AND A.TYPE = :swipeType ) t",
+                    " AND  A.swiper_id = :userId AND A.TYPE = :swipeType AND B.TYPE = :swipeType) t",
             nativeQuery = true)
     Page<BigInteger> findSwipersByType(Long userId, String swipeType, Pageable pageable
     );
@@ -73,5 +73,40 @@ public interface SwipeRepository extends BaseRepository<Swipe, Long> {
             nativeQuery = true
     )
     List<BigInteger> findSwipeSuggestions(Long userId, List<String> genderPreferenceList, int limit);
+
+    @Query(
+            value
+                    = "SELECT U.* " +
+                    " FROM user U  " +
+                    " INNER JOIN swipe SW " +
+                    " ON U.id = SW.swiper_id " +
+                    " WHERE  " +
+                    " U.deleted = 0  " +
+                    " AND U.active = 1  " +
+                    " AND U.suspended = 0  " +
+                    " AND SW.swipee_id = :userId " +
+                    " AND :userId NOT IN (SELECT swiper_id FROM swipe WHERE swipee_id = U.id)  ",
+            nativeQuery = true
+    )
+    List<BigInteger> findAdmirers(Long userId);
+
+
+    @Query(
+            value
+                    = "SELECT U.* " +
+                    " FROM user U  " +
+                    " INNER JOIN swipe SW " +
+                    " ON U.id = SW.swiper_id " +
+                    " WHERE  " +
+                    " U.deleted = 0  " +
+                    " AND U.active = 1  " +
+                    " AND U.suspended = 0  " +
+                    " AND SW.swipee_id = :userId " +
+                    " AND :userId NOT IN (SELECT swiper_id FROM swipe WHERE swipee_id = U.id)  " +
+                    "AND U.gender IN :genderPreferenceList " +
+                    "LIMIT :limit",
+            nativeQuery = true
+    )
+    List<BigInteger> findAdmirersWithPreferences(Long userId, List<String> genderPreferenceList, int limit);
 
 }
