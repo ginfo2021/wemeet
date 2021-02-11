3336e76a92fc31fea6b164539e4962516667dbef
udoriekpin <udorifriday@gmail.com>
2021-02-11 21:49:15 +0100

    phone number max and min digits

--- src/main/java/com/wemeet/dating/model/user/UserSignup.java
+++ src/main/java/com/wemeet/dating/model/user/UserSignup.java
@@ -1,12 +1,12 @@
 package com.wemeet.dating.model.user;
 
 
-import com.wemeet.dating.model.enums.Gender;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
@@ -22,6 +22,7 @@ public class UserSignup {
     private String lastName;
 
     @NotNull
+    @Size(min = 11, max = 11, message = "Minumum and maximum of 11 characters")
     private String userName;
 
     @NotBlank
