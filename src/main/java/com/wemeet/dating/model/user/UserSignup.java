ff95c9976e412b073a007dd8033cde89f44b9942
wemeet-app <69844126+wemeet-app@users.noreply.github.com>
2021-07-23 18:06:31 +0530

    Update UserSignup.java

--- src/main/java/com/wemeet/dating/model/user/UserSignup.java
+++ src/main/java/com/wemeet/dating/model/user/UserSignup.java
@@ -40,4 +40,13 @@ public class UserSignup {
     private Double longitude;
     private Double latitude;
     private Boolean active;
+    
+    
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
